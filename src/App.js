4b6e77bfa866f114e4753eadce3d042c7bc26b90
RegiAlves <regilene226@gmail.com>
2021-12-06 00:53:41 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Container, Head, BemVindo } from './style.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container >
+      <Head>  
+        <a href>Projeto Styled</a>
+      </Head>
+      <BemVindo cor="00FF00" tamanho={35} >Bem vindo ao sistema</BemVindo>
+    </Container>
   );
 }
 
